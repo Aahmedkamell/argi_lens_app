d43af8d414fda8e16b0c0e20f57abf6c9c23dc24
BaselElazaly <basel.elazaly2003@gmail.com>
2025-02-20 00:06:18 +0200

    repair boardin screen

--- lib/modules/splash/splash_screen.dart
+++ lib/modules/splash/splash_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:agre_lens_app/layout/app_layout.dart';
 import 'package:flutter/material.dart';
+import '../Boardina/boardina1_screen.dart';
 import '../home/home_screen.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -16,7 +17,7 @@ class _SplashScreenState extends State<SplashScreen> {
       Navigator.pushReplacement(
         context,
         PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) => AppLayout(),
+          pageBuilder: (context, animation, secondaryAnimation) => Boardina1Screen(),
           transitionsBuilder: (context, animation, secondaryAnimation, child) {
             var curve = Curves.easeOut;
             var tween = Tween<double>(begin: 0.0, end: 1.0).chain(
